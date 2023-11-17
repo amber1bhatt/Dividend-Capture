04b28ea1cd58650a70bed4c38bf9516da2a052af
Amber Bhatt <amberb220@gmail.com>
2023-11-16 21:46:22 -0800

    fixed issue with api change

--- service/src/application/providers/stock.provider.ts
+++ service/src/application/providers/stock.provider.ts
@@ -25,10 +25,37 @@ interface StockLastSalePrice {
 
 class StockProvider {
   public async getStocks(input: GetStocksInput): Promise<StockResults | null> {
-    const returnValues = await axios.get(
-      //YYYY-MM-DD
-      `https://api.nasdaq.com/api/calendar/dividends?date=${input.date}`
-    );
+    let returnValues;
+    try {
+      // returnValues = await axios.get(
+      //   //YYYY-MM-DD
+      //   `https://api.nasdaq.com/api/calendar/dividends?date=${input.date}`
+      // );
+      returnValues = await axios.get(
+        `https://api.nasdaq.com/api/calendar/dividends?date=${input.date}`,
+        {
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            Origin: "https://www.nasdaq.com",
+            Referer: "https://www.nasdaq.com/",
+            "Sec-Ch-Ua":
+              '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+          },
+        }
+      );
+      console.log(returnValues);
+    } catch (error) {
+      console.log(error);
+    }
 
     if (!returnValues) {
       return null;
@@ -49,7 +76,25 @@ class StockProvider {
     input: GetStockPriceInput
   ): Promise<StockLastSalePrice> {
     const returnValues = await axios.get(
-      `https://api.nasdaq.com/api/quote/${input.symbol}/info?assetclass=stocks`
+      `https://api.nasdaq.com/api/quote/${input.symbol}/info?assetclass=stocks`,
+      {
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Accept-Language": "en-US,en;q=0.9",
+          Origin: "https://www.nasdaq.com",
+          Referer: "https://www.nasdaq.com/",
+          "Sec-Ch-Ua":
+            '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+          "Sec-Ch-Ua-Mobile": "?0",
+          "Sec-Ch-Ua-Platform": '"Windows"',
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-site",
+        },
+      }
     );
 
     // console.log(returnValues.data.data.primaryData);
